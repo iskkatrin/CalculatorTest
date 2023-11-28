e8b61d07f21bd771500380628223152be229ccc0
Ekaterina <klarina2409@gmail.com>
2023-11-28 19:28:16 +0300

    hw

---
+++ src/test/java/Calculatorandtest/Calculatortest/CalculatorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package Calculatorandtest.Calculatortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
